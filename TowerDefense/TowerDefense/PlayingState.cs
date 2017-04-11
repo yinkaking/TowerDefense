4629ed184c671c88caf36d67bee8d95f6557393a
SoaringDylan <dylan@fampiera.nl>
2017-04-11 15:12:07 +0200

    Step 2

--- TowerDefense/TowerDefense/PlayingState.cs
+++ TowerDefense/TowerDefense/PlayingState.cs
@@ -10,15 +10,24 @@ namespace TowerDefense
     {
 
         SpriteGameObject homeBase;
+        GameObjectList ufos;
 
         public PlayingState()
         {
+            ufos = new GameObjectList();
             homeBase = new SpriteGameObject("spr_homebase");
             homeBase.Origin = homeBase.Sprite.Center;
             homeBase.Position = new Vector2(900, 340);
 
+            ufos.Add(new Ufo(new Vector2(20, 20), this.homeBase));
+            ufos.Add(new Ufo(new Vector2(-100, 600), this.homeBase));
+            ufos.Add(new Ufo(new Vector2(-300, 300), this.homeBase));
+            ufos.Add(new Ufo(new Vector2(800, -500), this.homeBase));
+            ufos.Add(new Ufo(new Vector2(700, 1000), this.homeBase));
+
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(homeBase);
+            this.Add(ufos);
         }
     }
 }
---
+++ TowerDefense/TowerDefense/Ufo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefense
+{
+    class Ufo : RotatingSpriteGameObject
+    {
+
+
+        public Ufo(Vector2 startPos, GameObject targetObject, string assetname = "spr_ufo", int layer = 0, string id = "", int sheetIndex = 0) : base(assetname, layer, id, sheetIndex)
+        {
+            this.Origin = this.Sprite.Center;
+            this.Position = startPos;
+            this.targetObject = targetObject;
+            this.LookAt(this.targetObject);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            this.Velocity = (AngularDirection * 50);
+        }
+    }
+}
