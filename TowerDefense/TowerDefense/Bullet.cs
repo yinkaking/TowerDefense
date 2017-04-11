d74ee82d4c1788f01d3c0f1a3a4acac97eb40ade
SoaringDylan <dylan@fampiera.nl>
2017-04-11 15:21:21 +0200

    Step 4

---
+++ TowerDefense/TowerDefense/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefense
+{
+    class Bullet : SpriteGameObject
+    {
+        public Bullet(Vector2 startPos, Vector2 startSpe, string assetname = "spr_bullet", int layer = 0, string id = "", int sheetIndex = 0) : base(assetname, layer, id, sheetIndex)
+        {
+            this.Origin = this.Sprite.Center;
+            this.Position = startPos;
+            this.Velocity = startSpe;
+        }
+    }
+}
--- TowerDefense/TowerDefense/PlayingState.cs
+++ TowerDefense/TowerDefense/PlayingState.cs
@@ -10,12 +10,13 @@ namespace TowerDefense
     {
 
         SpriteGameObject homeBase;
-        GameObjectList ufos, cannons;
+        GameObjectList ufos, cannons, bullets;
 
         public PlayingState()
         {
             ufos = new GameObjectList();
             cannons = new GameObjectList();
+            bullets = new GameObjectList();
             homeBase = new SpriteGameObject("spr_homebase");
             homeBase.Origin = homeBase.Sprite.Center;
             homeBase.Position = new Vector2(900, 340);
@@ -29,6 +30,7 @@ namespace TowerDefense
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(homeBase);
             this.Add(ufos);
+            this.Add(bullets);
             this.Add(cannons);
         }
 
@@ -38,6 +40,18 @@ namespace TowerDefense
             if(inputHelper.MouseLeftButtonPressed())
             {
                 cannons.Add(new AutoFireCannon(inputHelper.MousePosition));
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            foreach (AutoFireCannon item in cannons.Objects)
+            {
+                if (item.HasFired)
+                {
+                    this.bullets.Add(new Bullet(item.Position, (item.AngularDirection * 120)));
+                }
             }
         }
     }
